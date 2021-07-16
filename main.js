6d499aa1bf9955d8027ab567566263a8cb93b542
nicholasdadkins <nicholasdonadkins@gmail.com>
2021-07-15 23:49:41 -0400

    Displayed image output on main page.

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-const apiKey = 'iurA6At0M8o4XU1nZoHvkjxHuAsk1sGu'
-
 window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
 
+function apiRequest() {
+  var userInput = document.getElementById('search-input').value;
+  console.log(userInput);
+  var apiKey = "iurA6At0M8o4XU1nZoHvkjxHuAsk1sGu"
+  var url = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + userInput + '&limit=25&offset=0&rating=g&lang=en';
 
-  let fetchResults = (searchQuery) => {
-    // write code here to perform a fetch call the the GIPHY API
-    // once the fetch returns some data, pass the data to the displayResults()
-
-    // displayResults(response)
-  }
-
-  let displayResults = (response) => {
-    // Write code to loop over the results and show the results on the page
-    // this will also involve being able to console log out the results and figuring out the result structure. Just like the Youtube API
-  }
+  fetch(url)
+  .then(function (data) {
+    return data.json();
+  })
+  .then(function (json) {
+    document.querySelector('.search-results').innerHTML = ""
+    for (let i = 0; i < json.data.length; i++) {
+      var imgPath = json.data[i].images.fixed_height.url;
+      var img = document.createElement("img");
+      img.setAttribute("src", imgPath);
+      document.querySelector('.search-results').appendChild(img);
+    }
+  });
+}
 
   document.getElementById('search-button').addEventListener('click', () => {
     console.log('the search button was clicked!')
-    let searchQuery = document.getElementById('search-input').value
-    console.log('the value of the searchQuery is', searchQuery)
-
-    fetchResults(searchQuery)
+    apiRequest()
 
   })
-
 
 });
