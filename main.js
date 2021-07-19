6cc580f51b026458c06b4f800443e86558050300
nicholasdadkins <nicholasdonadkins@gmail.com>
2021-07-19 02:43:56 -0400

    Added flex container for search results

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
 
 function apiRequest() {
-  var userInput = document.getElementById('search-input').value;
+  let userInput = document.getElementById('search-input').value;
   console.log(userInput);
-  var apiKey = "iurA6At0M8o4XU1nZoHvkjxHuAsk1sGu"
-  var url = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + userInput + '&limit=25&offset=0&rating=g&lang=en';
+  let apiKey = "iurA6At0M8o4XU1nZoHvkjxHuAsk1sGu"
+  let url = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + userInput + '&limit=25&offset=0&rating=g&lang=en';
 
   fetch(url)
   .then(function (data) {
@@ -14,8 +14,8 @@ function apiRequest() {
   .then(function (json) {
     document.querySelector('.search-results').innerHTML = ""
     for (let i = 0; i < json.data.length; i++) {
-      var imgPath = json.data[i].images.fixed_height.url;
-      var img = document.createElement("img");
+      let imgPath = json.data[i].images.fixed_height.url;
+      let img = document.createElement("img");
       img.setAttribute("src", imgPath);
       document.querySelector('.search-results').appendChild(img);
     }
