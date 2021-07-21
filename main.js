01b5b576e89eecc58f349b149032d52562aa4d99
nicholasdadkins <nicholasdonadkins@gmail.com>
2021-07-21 19:04:53 -0400

    Made fetch async and applied tailwind cols

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ function apiRequest() {
   let apiKey = "iurA6At0M8o4XU1nZoHvkjxHuAsk1sGu"
   let url = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + userInput + '&limit=25&offset=0&rating=g&lang=en';
 
-  fetch(url)
-  .then(function (data) {
-    return data.json();
-  })
-  .then(function (json) {
-    document.querySelector('.search-results').innerHTML = ""
+  let myFunc = async () => {
+    let res = await fetch(url)
+    let json = await res.json()
+
+    
+    console.log('the data from async await is', json)
+
+    document.getElementById('search-results').innerHTML = ""
     for (let i = 0; i < json.data.length; i++) {
       let imgPath = json.data[i].images.fixed_height.url;
       let img = document.createElement("img");
       img.setAttribute("src", imgPath);
-      document.querySelector('.search-results').appendChild(img);
-    }
-  });
+      document.getElementById('search-results').appendChild(img);
+  }   
+  }
+  myFunc();
 }
   document.getElementById('search-button').addEventListener('click', () => {
     console.log('the search button was clicked!')
